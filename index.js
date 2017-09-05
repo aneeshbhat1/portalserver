cd668b7680e4db42757deeec07ac149bff5d0be8
Parthi <parthiban.sudhaman@gmail.com>
2017-09-05 08:37:01 +0530

    post build configured

--- index.js
+++ index.js
@@ -22,5 +22,15 @@ app.use(passport.session())
 
 require("./routes/authRoutes")(app)
 
+if (process.env.NODE_ENV === "production") {
+  // Express to serve static files
+  app.use(express.static("client/build"))
+  // Express to serve index.html, when it is not able to recognize the route
+  const path = require("path")
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 const PORT = process.env.PORT || 7000
 app.listen(PORT)
